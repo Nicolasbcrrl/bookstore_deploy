6ca7c387b07db7a35ba6a0e104014e06590d9948
Nicolasbcrrl <71258958+Nicolasbcrrl@users.noreply.github.com>
2022-10-17 13:18:31 +0200

    Delete src directory

--- src/main/java/com/serverprogramming/bookstorev2/domain/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.serverprogramming.bookstorev2.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface BookRepository extends CrudRepository<Book, String> {
-    List<Book> findByIsbn(String isbn);
-}
--- src/main/java/com/serverprogramming/bookstorev2/domain/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.serverprogramming.bookstorev2.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-    Category findByName(String name);
-}
